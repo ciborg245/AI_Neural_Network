0e120fcdad1871d93b5739608de3e519814ad77d
Alejandro <alejandrochgu@hotmail.com>
2019-05-09 15:46:22 -0600

    finished, learning rate and hidden layer neurons still in test

--- main.py
+++ main.py
@@ -1,38 +1,99 @@
 import network
-import random
+from math import sqrt, trunc
 import numpy as np
-
-# x = np.array([[[1], [2]], [[1], [2]], [[1], [2]]])
-# x = np.array([[1, 2], [1, 2], [1, 2]])
+from PIL import Image
 
 
+layer2Len = 66
+layer3Len = 10
+# layer4Len = 10
 
-# x = np.array([[1], [2]])
-# H2 = np.array([
-#     [0.1, 0.3, 0.5],
-#     [0.2, 0.4, 0.6]
-#     ])
-# H3 = np.array([0.7, 0.8, 0.9])
-#
-# res = network.feed_forward(x, H2, H3)
-# print(res)
+H1 = np.array([np.random.uniform(0, 0.18, 784) for j in range(layer2Len)])
+H2 = np.array([np.random.uniform(0, 0.18, layer2Len) for j in range(layer3Len)])
+# B2 = np.ones(layer2Len)
+# H3 = np.array([np.random.uniform(0, 0.001, layer3Len) for j in range(layer4Len)])
+# B3 = np.ones(layer3Len)
+weights = [H1, H2]
+# biases = [B2, B3]
 
+# circle = np.load('data/full_numpy_bitmap_circle.npy')[:5000]
+# face = np.load('data/full_numpy_bitmap_face.npy')[:5000]
+# house = np.load('data/full_numpy_bitmap_house.npy')[:5000]
+# square = np.load('data/full_numpy_bitmap_square.npy')[:5000]
+# tree = np.load('data/full_numpy_bitmap_tree.npy')[:5000]
+# triangle = np.load('data/full_numpy_bitmap_triangle.npy')[:5000]
+# mickey = np.load('data/full_numpy_bitmap_panda.npy')[:5000]
 
-layer2Len = 250
-layer3Len = 250
-# H2 = np.array([[random.uniform(0, 1) for i in range(784)] for j in range(layer2Len)])
-# H3 = np.array([[random.uniform(0, 1) for i in range(layer2Len)] for j in range(layer3Len)])
-H2 = np.array([[random.uniform(0, 1) for i in range(784)] for j in range(layer2Len)])
-H3 = np.array([[random.uniform(0, 1) for i in range(layer2Len)] for j in range(layer3Len)])
-# print(H2)
-# print(H2.shape)
+circle = np.load('data/full_numpy_bitmap_circle.npy')[:100000]
+face = np.load('data/full_numpy_bitmap_face.npy')
+house = np.load('data/full_numpy_bitmap_house.npy')
+square = np.load('data/full_numpy_bitmap_square.npy')
+tree = np.load('data/full_numpy_bitmap_tree.npy')
+triangle = np.load('data/full_numpy_bitmap_triangle.npy')
+# mickey = np.load('data/full_numpy_bitmap_panda.npy')
+mickey = np.load('data/mickeymouse.npy')
+egg = np.load('data/egg.npy')
+question = np.load('data/questionmark.npy')
+sadface = np.load('data/sadface.npy')
 
-a = np.load('data/full_numpy_bitmap_circle.npy')
-a = a[:5000]
-# print(len(a))
-print(network.feed_forward(a, H2, H3))
-# print(len(a[0]))
-# print(len(a))
-
+data = np.concatenate((circle, face, house, square, tree, triangle, mickey, egg, question, sadface))
+data[data > 1] = 1
 # for i in range(28):
-#     print([a[0][i*28+j] for j in range(28)])
+#     print([dataset[1200][i*28 + j] for j in range(28)])
+print(data.shape)
+
+results = np.concatenate((
+    np.repeat(0, len(circle)),
+    np.repeat(1, len(face)),
+    np.repeat(2, len(house)),
+    np.repeat(3, len(square)),
+    np.repeat(4, len(tree)),
+    np.repeat(5, len(triangle)),
+    np.repeat(6, len(mickey)),
+    np.repeat(7, len(egg)),
+    np.repeat(8, len(question)),
+    np.repeat(9, len(sadface)),
+    ))
+
+dataset = list(map(lambda x, y: (x, y), data, results))
+# dataset = (data, results)
+np.random.shuffle(dataset)
+# dataset = dataset[:10]
+
+trainingLen = trunc(len(dataset) * 0.7)
+for i in range(0, trainingLen, 10):
+    batch = dataset[i:i+10]
+    delta = network.back_propagation(batch, weights)
+    weights = weights - (1.0 / 10)*delta
+    print(i)
+
+np.save('data/weights', weights)
+
+testLen = trunc(len(dataset) * 0.85)
+test = dataset[testLen:]
+cont = 0
+for i in test:
+    if i[1] == np.argmax(network.feed_forward(np.array([i[0]]), weights)[2]):
+        cont += 1
+
+print('{0} / {1}'.format(cont, len(test)))
+
+# network.feed_forward(dataset, weights)
+
+# delta = network.back_propagation(dataset, weights)
+# print(weights[1])
+#
+# new_weights = weights - (0.1 / len(dataset))*delta
+# print(delta[0].shape)
+# print(new_weights[0].shape)
+
+file = Image.open("input.bmp")
+input = np.array(file)
+inputArr = [0 if input[i][j][0] == 255 else 1 for i in range(28) for j in range(28)]
+# for i in range(28):
+#     print([inputArr[i*28 + j] for j in range(28)])
+# print(inputArr)
+
+res = network.feed_forward(np.array([inputArr]), weights)
+print(res[2])
+resLabels = ['circle', 'face', 'house', 'square', 'tree', 'triangle', 'mickey', 'egg', 'question', 'sadface']
--- network.py
+++ network.py
@@ -6,66 +6,96 @@ def sig(x):
 
 sigmoide = np.vectorize(sig)
 
-# def feed_forward(x, H1, H2):
-#     sigmoide = np.vectorize(sig)
-#
-#     m, n = x.shape
-#     # print(x)
-#     # print(np.ones(m))
-#     # print(np.ones(n).reshape(n, 1))
-#     A1 = np.hstack((
-#         np.ones(m).reshape(m, 1),
-#         x
-#     ))
-#
-#     print(A1)
-#
-    # print(x)
-    # print(H1)
-    # a1 = np.vstack((1, x))
-    #
-    # print(H1.shape)
-    # z2 = np.matmul(H1, a1)
-    #
-    # a2 = sigmoide(z2)
-    # print(a2)
-    # a2 = np.vstack((1, a2))
-    #
-    # z3 = np.matmul(H2, a2)
-    #
-    # a3 = sigmoide(z3)
-    #
-    # return a3
-
 def feed_forward(x, theta):
     thetaLen = len(theta)
-    print(x.shape)
-    print(theta1.shape)
+
     x = x.T
-    z = []
-    a = [x]
+    Z = []
+    A = [x]
     for i in range(thetaLen):
-        Z.append(
-            theta[i] @ a[i]
-        )
-        a_asdf = sigmoide(z[i])
+        # print(A[i].shape)
+        if i > 0:
+            m, n = theta[i].shape
+            # print('theta')
+            # print(theta[i].shape)
 
-        if i < thetaLen - 2:
-            m, n = a_asdf.shape
-            A.append(
-                np.vstack((
-                    np.ones(n),
-                    a_asdf
-                ))
-            )
+            theta_mod = np.hstack((
+                np.ones(m).reshape(m, 1),
+                theta[i]
+            ))
+            # print('theta mod')
+            # print(theta_mod.shape)
         else:
-            A.append(a_Asdf)
+            theta_mod = np.array(theta[i])
+        z_calc = theta_mod @ A[i]
+        Z.append(z_calc)
+        # print('z calculated')
 
-    return A[-1:]
+        a_calc = sigmoide(Z[i])
+        # print('sigmoid calculated')
 
-    # z2 = theta1 @ x
-    # print(z2.shape)
-    # for
-    # print('z2 finished')
-    # a2 = sigmoide(z2)
-    # print('sigmoid finished')
+        if i < thetaLen - 1:
+            m, n = a_calc.shape
+            # print(a_calc.shape)
+            a_calc = np.vstack((
+                np.ones(n),
+                a_calc
+            ))
+        A.append(a_calc)
+
+    for i in range(1, len(A)-1):
+        A[i] = A[i][1:]
+    # print(A[1].shape)
+    return np.array(A)
+
+def getY(index):
+    arr = np.zeros(10)
+    arr[index] = 1.0
+    return arr
+
+def back_propagation(input, theta):
+    m = len(theta)
+    x = np.array([el[0] for el in input])
+    # print(x)
+    # print(input)
+    A = feed_forward(x, theta)
+
+    delta = np.array([np.zeros(theta[i].shape) for i in range(m)])
+    # print(delta[0].shape)
+    # print(delta[1].shape)
+    # y = [
+    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    # ]
+    for n in range(len(x)):
+    # for n in range(1):
+        error = []
+        for i in range(1, m+1):
+            if i == 1:
+                error.append(
+                    np.array(A[m-i+1].T[n] - getY(input[i][1]))
+                )
+
+            else:
+                error.append(calculate_error(theta[m-i+1], error[i-2], A[m-i+1].T[n]))
+            # print(np.array([error[i-1]]).T.shape)
+            # print(np.array([A[m-i].T[n]]).shape)
+            # print((np.array([error[i-1]]).T @ np.array([A[m-i+1].T[n]])).shape)
+            delta[m-i] = delta[m-i] + (np.array([error[i-1]]).T @ np.array([A[m-i].T[n]]))
+
+    # print(error[0].shape)
+    # print(error[1].shape)
+    # print(A[0].T[0])
+    # print(delta[0][0])
+
+    return delta
+
+def calculate_error(theta, sub_error, A):
+    # print((theta.T @ sub_error) * (A * (np.ones(len(A))-A)))
+    # print((theta.T @ sub_error) * (A * (np.ones(len(A))-A)))
+    return np.array((theta.T @ sub_error) * (A * (np.ones(len(A))-A)))
